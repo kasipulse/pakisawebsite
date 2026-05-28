9b98fd58a6c072712528ddf3f5c98927dd76e41b
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-05-28 21:07:52 +0200

    Create sw.js

---
+++ fuel/sw.js
@@ -0,0 +1,11 @@
+const CACHE_NAME = "pakisa-fuel-v1";
+
+const urlsToCache = [
+  "/fuel/",
+  "/fuel/dashboard.html",
+  "/fuel/manifest.json",
+  "/fuel/icons/icon-192.png",
+  "/fuel/icons/icon-512.png"
+];
+
+self.addEventListener("install
