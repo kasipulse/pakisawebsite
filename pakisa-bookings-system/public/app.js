c5b7934e9562ccfe5c8a6e7580dbcb2e4b3031c5
kasipulse <148783028+kasipulse@users.noreply.github.com>
2026-06-29 16:45:15 +0200

    Update app.js

--- pakisa-bookings-system/public/app.js
+++ pakisa-bookings-system/public/app.js
@@ -1,1 +1,14 @@
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("App initialized");
 
+    const emailLoginBtn = document.getElementById('email-login');
+    const googleLoginBtn = document.getElementById('google-login-btn');
+
+    emailLoginBtn.addEventListener('click', () => {
+        alert("Email login logic goes here");
+    });
+
+    googleLoginBtn.addEventListener('click', () => {
+        alert("Google login logic goes here");
+    });
+});
